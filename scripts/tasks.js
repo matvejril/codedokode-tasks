0d6373fe38d4bcfb9db44def6c164d9a9fabfd86
matvejril <matvej.ril@gmail.com>
2018-12-02 16:20:34 +0700

    Добавлены 3,4 задачи

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -17,10 +17,12 @@ window.addEventListener('load', function() {
     var generator1 = sequence(10, 3);
     var generator2 = sequence(7, 1);
 
+    // result
     console.log("----- Задача 1 ----- \n\n");
     console.log("gen1", generator1());
     console.log("gen2", generator2());
     console.log("gen1", generator1());
+    console.log("\n");
 
     // ЗАДАЧА 2
     function take(fn, count) {
@@ -32,14 +34,52 @@ window.addEventListener('load', function() {
     }
     var gen2  = sequence(0, 2);
 
+    // result
     console.log("----- Задача 2 ----- \n\n");
     console.log(take(gen2 , 5));
+    console.log("\n");
 
     // ЗАДАЧА 3
+    function map (fn, array) {
+        var newArr = [];
+        for (var i = 0; i < array.length; i++) {
+            newArr.push(fn(array[i]));
+        }
+        return newArr
+    }
+    function square(x) {
+        return x * x;
+    }
 
-
+    // result
+    console.log("----- Задача 3 ----- \n\n");
+    console.log(map(square, [1, 2, 3, 4]));
+    console.log(map(square, []));
+    console.log("\n");
 
     // ЗАДАЧА 4
+    function fmap(a, b) {
+        return function() {
+            var args = [];
+            for (var j = 0; j < arguments.length; j++) {
+                args.push(arguments[j]);
+            }
+            return a(b.apply(null, args));
+        }
+    }
+    function square(x) {
+        return x * x;
+    }
+    function add(a, b) {
+        return a + b;
+    }
+    var squareAdd = fmap(square, add);
+
+    // result
+    console.log("----- Задача 4 ----- \n\n");
+    console.log(squareAdd(2, 3)); // 25 = (2 + 3) ^ 2
+    console.log(squareAdd(5, 7)); // 144 = (5 + 7) ^ 2
+    console.log("\n");
 
     // ЗАДАЧА 5
 
